956c75a665c0a75b0c06bd65381903889f4a2505
kgrowkgrow <kevingrow11@gmail.com>
2020-10-09 09:37:16 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+
+    @@all = []
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        self.save
+        # binding.pry
+    end 
+
+    def self.all
+        @@all
+        # binding.pry
+    end 
+
+    def self.clear_all
+        @@all.clear
+    end 
+
+    def self.print_all
+        @@all.map {|name| puts name.name}
+    end 
+
+    def save 
+        @@all << self
+    end 
+
+
+
+
+end 
